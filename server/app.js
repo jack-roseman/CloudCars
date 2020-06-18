26b5649e48dfa682eb890c1bfe19a6eca11b4498
Jack Roseman <jackroseman33@yahoo.com>
2020-06-18 13:13:38 -0400

    deploying

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 const HOST = 'localhost'
 var mongoose = require('mongoose');
 const Vehicle = require('./api/models/Vehicle.js');
