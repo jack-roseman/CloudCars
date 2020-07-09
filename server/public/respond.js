f6cd89828b730cd739e1eb7d8af8c7853a862b48
Jack Roseman <jackroseman33@yahoo.com>
2020-07-09 09:05:17 -0400

    fixed bug #1

--- server/public/respond.js
+++ server/public/respond.js
@@ -13,10 +13,13 @@ socket.on('connectionStateChange', (connectionState) => {
 socket.on('classificationTaskChange', (tasks) => {
     queue = new PriorityQueue({ initialValues: tasks, comparator: function(a, b) { return a.id - b.id; }});
     if (queue.length != 0) {
-        const task = queue.dequeue();
+        let task = queue.peek();
         localStorage.setItem('current_task', JSON.stringify(task));
         $("#classify_image").attr('src', task.imgUrl);
         $("#classify-div").show()
+    } else {
+        $("#classify_image").attr('src', '');
+        $("#classify-div").hide()
     }
 });
 
@@ -27,6 +30,7 @@ $(document).ready(() => {
 
 function classify(cl) {
     const currentTask = JSON.parse(localStorage.getItem('current_task'));
+    queue.dequeue();
     socket.emit('classification_completion', {
         id:  currentTask.id,
         imgUrl:  currentTask.imgUrl,
